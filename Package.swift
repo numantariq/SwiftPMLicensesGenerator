b9d2981ec2e60efc4e7cc078120b02ab6d13e03d
Nouman Tariq <numantariq@gmail.com>
2021-10-25 11:11:52 +0200

    Downgraded Swift tools to 5.3

--- Package.swift
+++ Package.swift
@@ -1,10 +1,13 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "SwiftPMLicensesGenerator",
+    products: [
+        .executable(name: "SwiftPMLicensesGenerator", targets: ["SwiftPMLicensesGenerator"])
+    ],
     dependencies: [
         .package(
             url: "https://github.com/apple/swift-argument-parser",
@@ -18,7 +21,7 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .executableTarget(
+        .target(
             name: "SwiftPMLicensesGenerator",
             dependencies: [
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
